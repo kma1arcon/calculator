074970caf02ebe1fb8fc67c9979d3b949fd00308
KM <kmalarcon.freelancer@gmail.com>
2026-03-25 23:16:46 +0800

    added while loop so that user can repeat operations

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 def add(a,b):
- return a+b
+    return a+b
 
 def subtract(a,b):
- return a-b
+    return a-b
 
 def multiply(a,b):
- return a*b
+    return a*b
 
 def divide(a,b):
- if b==0:
-  return"Error: Cannot divide by zero"
- return a/b
+    if b==0:
+        return"Error: Cannot divide by zero"
+    return a/b
 
+while True:
+    print("Select operation: +, -, *, /")
+    choice=input("Enter choice: ")
 
-print("Select operation: +, -, *, /")
-choice=input("Enter choice: ")
+    num1=float(input("Enter first number: "))
+    num2=float(input("Enter second number: "))
 
-num1=float(input("Enter first number: "))
-num2=float(input("Enter second number: "))
+    if choice=="+":
+        print(add(num1,num2))
+    elif choice=="-":
+        print(subtract(num1,num2))
+    elif choice=="*":
+        print(multiply(num1,num2))
+    elif choice=="/":
+        print(divide(num1,num2))
+    else:
+        print("Invalid input")
 
-if choice=="+":
- print(add(num1,num2))
-elif choice=="-":
- print(subtract(num1,num2))
-elif choice=="*":
- print(multiply(num1,num2))
-elif choice=="/":
- print(divide(num1,num2))
-else:
- print("Invalid input")
+    again = input("Do you want to perform another operation? (y/n): ")
+    if again.lower() != 'y':
+        break
